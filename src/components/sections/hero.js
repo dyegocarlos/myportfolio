7e28de0102b7920e54f095845c5740af31515672
dyegocarlos <dyego12345@gmail.com>
2021-06-17 21:10:19 -0300

    first

---
+++ src/components/sections/hero.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styled from 'styled-components';
+import { email } from '@config';
+import { navDelay, loaderDelay } from '@utils';
+import { usePrefersReducedMotion } from '@hooks';
+// import TypeIt from "typeit";
+
+const StyledHeroSection = styled.section`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  padding: 0;
+
+  @media (max-width: 480px) and (min-height: 700px) {
+    padding-bottom: 10vh;
+  }
+
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+    font-weight: 400;
+
+    @media (max-width: 480px) {
+      margin: 0 0 20px 2px;
+    }
+  }
+
+  h3 {
+    margin-top: 10px;
+    color: var(--slate);
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 20px 0 0;
+    max-width: 500px;
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Hero = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const one = <h1>Hi, my name is</h1>;
+  const two = (
+    <h2 id="myname" className="big-heading">
+      Dyego Morais.
+    </h2>
+  );
+
+  // const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">I'm a web artisan.</h3>;
+  const four = (
+    <p>
+      I'm a Recife-based full stack designer who specializes in design and front-end development
+      exceptional digital experiences. Moreover, I'm a PhD researcher focused on child participation
+      in participatory design of digital artifacts.
+    </p>
+  );
+  const five = (
+    <a href={`mailto:${email}`} className="email-link">
+      Get In Touch
+    </a>
+  );
+
+  const items = [one, two, three, four, five];
+
+  return (
+    <StyledHeroSection>
+      {prefersReducedMotion ? (
+        <>
+          {items.map((item, i) => (
+            <div key={i}>{item}</div>
+          ))}
+        </>
+      ) : (
+        <TransitionGroup component={null}>
+          {isMounted &&
+            items.map((item, i) => (
+              <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
+                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+              </CSSTransition>
+            ))}
+        </TransitionGroup>
+      )}
+    </StyledHeroSection>
+  );
+};
+
+export default Hero;
